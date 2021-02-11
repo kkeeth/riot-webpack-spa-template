de09a6f8c8aa5b623740895fd544ccfd83411444
kkeeth <zensin0082@gmail.com>
2021-02-11 21:41:51 +0900

    chore: added public directory

---
+++ public/chunks/pages/about/index.33cfb935331b177727fe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{19:function(e,n,t){"use strict";t.r(n),n.default={css:null,exports:null,template:function(e,n,t,o){return e("<h1>Something about me</h1><p>Here I describe who I am and what I do</p>",[])},name:"about"}}}]);
+//# sourceMappingURL=index.33cfb935331b177727fe.js.map
